cbc8e0e23476e37978cdebebe7893b5845bb2c96
kanishkagarg27 <kanishkagargfasd@gmail.com>
2025-07-06 18:05:37 +0530

    Updated axios baseURL and removed unnecessary files

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://chatty-web-app-2.onrender.com/api",  // ← your backend URL
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
