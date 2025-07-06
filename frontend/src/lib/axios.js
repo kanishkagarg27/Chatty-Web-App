85da90f819f14268a688db4f82a113fe88f28055
kanishkagarg27 <kanishkagargfasd@gmail.com>
2025-07-06 19:40:37 +0530

    Backend url added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatty-app-hbq5.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatty-web-app-backend.onrender.com",
   withCredentials: true,
 });
